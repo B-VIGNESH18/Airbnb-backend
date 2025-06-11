3c1b508068d8f9663d4750873f0fc5856cdad713
B-VIGNESH18 <vigneshbalu382002@gmail.com>
2025-06-11 13:28:18 +0530

    update

--- index.js
+++ index.js
@@ -744,7 +744,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-frontend-blue-psi.vercel.app/",
   })
 );
 
